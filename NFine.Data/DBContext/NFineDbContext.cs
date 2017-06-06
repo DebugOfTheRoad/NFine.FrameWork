984356486a899aaa25737a19789623b147cb0887
Yill <hebutyll@outlook.com>
2017-06-07 07:06:48 +0800

    增加数据层的实现

---
+++ NFine.Data/DBContext/NFineDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace NFine.Data
+{
+    public class NFineDbContext : DbContext
+    {
+        public NFineDbContext() : base("NFineDbContext")
+        {
+
+        }
+    }
+}
---
+++ NFine.Data/Repository/IRepository.T.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFine.Data
+{
+    public interface IRepository<TEntity> where TEntity : class, new()
+    {
+        int Insert(TEntity entity);
+        int Insert(List<TEntity> entityList);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+        int Delete(Expression<Func<TEntity, bool>> predicate);
+        TEntity FindEntity(object keyValue);
+        TEntity FindEntity(Expression<Func<TEntity, bool>> predicate);
+        IQueryable<TEntity> Query();
+        IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> predicate);
+        List<TEntity> FindList(string strSql);
+        List<TEntity> FindList(string strSql, DbParameter[] dbParameters);
+    }
+}
